e6668e42675c977da6f413ae311d08db1191f830
Keith Aston <Keith.aston88@gmail.com>
2023-10-21 15:41:48 +0900

    Finished set customer info endpoint

--- src/main/java/com/ka/udemyspringboothackingcourse/helpers/MainHelper.java
+++ src/main/java/com/ka/udemyspringboothackingcourse/helpers/MainHelper.java
@@ -9,6 +9,9 @@ public class MainHelper {
     public static final String ADDRESS_COLUMN_HEADER = "address";
     public static final String CONTACT_NUMBER_COLUMN_HEADER = "contact_number";
 
+    public static final String USER_TYPE_ADMIN = "admin";
 
-
+    public static final String USER_DOES_NOT_EXIST = "User ID does not exist";
+    public static final String SQL_EXCEPTION = "Database connection problem, please contact support";
+    public static final String GENERIC_EXCEPTION = "Something went wrong. Please try again later";
 }
